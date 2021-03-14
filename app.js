542c9e944b9104a3e62e22ecc9e656059ed54993
Cody Trent <Creepercatsgaming@gmail.com>
2021-03-14 17:30:45 -0400

    Added some necessary files for Heroku

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const app = express();
 
 app.use(express.static('./public'));
 
+const PORT = process.env.PORT || 5000;
+
 app.get('/', (req, res) =>{
     res.sendFile('public/views/index.html', { root: __dirname });
 })
 
-app.listen('3000', () =>{
-    console.log('Listening on port 3000');
+app.listen(PORT, () =>{
+    console.log(`Listening on port ${PORT}`);
 })
